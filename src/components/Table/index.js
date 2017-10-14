a4ff63787dc9cc14ab6913eed9204990aea08d6b
Jed Davis <JDavis2759@gmail.com>
2017-10-13 21:32:49 -0500

    Added initial styling

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './Table.css';
 
 const Table = ({ list, width=320, height=180 }) =>
-  <div>
+  <div className="table-container">
     { list.map(item =>
-      <div key={item.id}>
+      <div className="table-item" key={item.id}>
       <img src={item.thumbnail_url.replace('{width}x{height}', `${width}x${height}`)}
            alt="Stream Thumbnail"
       />
-      <h3>{item.title}</h3>
-      <p>{item.viewer_count} viewers on user_id:{item.user_id}</p>
+      <div className="table-item-text">
+        <h3>{item.title}</h3>
+        <p>{item.viewer_count} viewers on user_id:{item.user_id}</p>
+      </div>
       </div>
     )}
   </div>
