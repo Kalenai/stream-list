9ea184e3943f44e39b6f61a490d2fcf474f8f246
Jed Davis <JDavis2759@gmail.com>
2017-10-15 18:29:51 -0500

    More styling - still slightly buggy.

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './Table.css';
 
 const Table = ({ list, width=320, height=180 }) =>
-  <div className="table-container">
+  <div className="table-container flex">
     { list.map(item =>
       <div className="table-item" key={item.id}>
-      <img src={item.thumbnail_url.replace('{width}x{height}', `${width}x${height}`)}
-           alt="Stream Thumbnail"
-      />
-      <div className="table-item-text">
-        <h3>{item.title}</h3>
-        <p>{item.viewer_count} viewers on user_id:{item.user_id}</p>
-      </div>
+        <a href="#">
+          <div className="flex">
+            <img src={item.thumbnail_url.replace('{width}x{height}', `${width}x${height}`)}
+                alt="Stream Thumbnail"
+            />
+            <div className="table-item-text">
+              <h3>{item.title}</h3>
+              <p>{item.viewer_count} viewers on user_id:{item.user_id}</p>
+            </div>
+          </div>
+        </a>
       </div>
     )}
   </div>
