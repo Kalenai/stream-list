aad50eba7c6211c5d48b527d288d9f9893c2c4e5
Jed Davis <JDavis2759@gmail.com>
2017-10-16 21:27:22 -0500

    Navbar component styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Navbar.css';
 
 const Navbar = ({ onClick }) =>
-  <div className="Navbar"></div>
+  <nav>
+    <div className="navbar">
+        <ul>
+            <li><a href="">Games</a></li>
+            <li><a href="">Communities</a></li>
+            <li><a href="">Popular</a></li>
+            <li><a href="">Creative</a></li>
+            <li><a href="">Discover</a></li>
+        </ul>
+    </div>
+  </nav>
 
 export default Navbar;
